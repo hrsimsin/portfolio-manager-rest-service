7f38d2156bf8c2f4a812bea4dbd4406d6a3a93c1
harsimar17290 <harsimar17290@iiitd.ac.in>
2021-06-11 12:59:14 +0530

    Implement user login

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Config = require('../config');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const ResponseUtils = require('../utils/response-utils');
 
 const getAuthRouter = () => {
-    return new Promise(async (resolve,reject)=>{
+    return new Promise(async (resolve, reject) => {
         const router = express.Router();
-        await mongoose.connect(Config.databaseUrl,{useNewUrlParser:true, useUnifiedTopology: true}).catch((reason)=>{reject(reason)});
+        await mongoose.connect(Config.databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true }).catch((reason) => { reject(reason) });
 
-        router.post('/signup',async(req,res)=>{
-            const {email,password,name} = req.body;
+        router.post('/signup', async (req, res) => {
+            const { email, password, name } = req.body;
+
             await User.create({
-                email,name,
-                password : await bcrypt.hash(password,10)
-            });
-            ResponseUtils.successMessage(res,'User signed up.');
+                email, name,
+                password: await bcrypt.hash(password, 10)
+            }).catch((reason) => { ResponseUtils.serverError(res); });
+
+            ResponseUtils.success(res, 'User signed up.');
+        });
+
+        router.post('/login', async (req, res) => {
+            const { email, password } = req.body;
+            try {
+                const user = await User.findOne({ email });
+                if (user && await bcrypt.compare(password, user.password)) {
+                    ResponseUtils.success(res,'Access Token Generated.',{
+                        accessToken : jwt.sign({email: user.email},Config.jwtSecret),
+                        name: user.name
+                    });
+                }
+                else{
+                    ResponseUtils.unauthorized(res,'Invalid email / password.');
+                }
+            }
+            catch (error) {
+                ResponseUtils.serverError(res);
+            }
         });
 
         resolve(router);
--- src/utils/response-utils.js
+++ src/utils/response-utils.js
@@ -1,7 +1,23 @@
 class ResponseUtils{
-    static successMessage = (res,message) => {
+    static success = (res,message = undefined ,data = undefined) => {
         res.status(200).json({
-            success:true,message
+            success:true,
+            message,
+            data
+        });
+    }
+
+    static unauthorized = (res,message = undefined) => {
+        res.status(401).json({
+            success:false,
+            message
+        });
+    }
+
+    static serverError = (res) => {
+        res.status(500).json({
+            success:false,
+            message:'Internal Server Error'
         });
     }
 }
